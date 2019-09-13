297c7d1a8794cf65f10133da7246de03a1d0eb19
Wahe3bru <wahe3bru@gmail.com>
2019-09-13 11:34:55 +0200

    add pic url

--- plant_listing.py
+++ plant_listing.py
@@ -1,15 +1,18 @@
 import re
 
+from bs4 import BeautifulSoup
 import pandas as pd
 from selenium import webdriver
-from bs4 import BeautifulSoup
 
 
 plants_dic = {}
 name_li = []
 descr_li = []
 price_li = []
+pic_li = []
 
+# windows pc
+# driver = webdriver.PhantomJS(executable_path=r'C:\Users\WaheebA\Documents\work\learnings\webscraping\webscraping_book\phantomjs-2.1.1-windows\bin\phantomjs')
 driver = webdriver.PhantomJS(executable_path='/Users/wahe3bru/Documents/phantomjs-2.1.1-macosx/bin/phantomjs')
 driver.get('https://livingseeds.co.za/heirloom-seedlings')
 
@@ -24,9 +27,13 @@ while next_page:
             name = plant.h4.get_text()
             description = plant.p.get_text()
             price = plant.find('p',{'class': 'price'}).get_text().strip()
+            plant_pic = plant.parent.parent.find('div', {'class': 'image'}).find(src=True)['src']
+
             name_li.append(name)
             descr_li.append(description)
             price_li.append(price)
+            pic_li.append(plant_pic)
+
     next_page = bs.find('li', {'class':'active'}).next_sibling
     if next_page:
         driver.get(next_page.find(href=True)['href'])
@@ -37,6 +44,8 @@ while next_page:
 plants_dic['seedling'] = name_li
 plants_dic['description'] = descr_li
 plants_dic['price'] = price_li
+plants_dic['pic_url'] = pic_li
+
 
 seedling_df = pd.DataFrame.from_dict(plants_dic)
 seedling_df['price'] = seedling_df.price.apply(lambda s: re.findall(r'^(R\d{1,2}\.\d\d)',s)[0])
